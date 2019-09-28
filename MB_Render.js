81cc77ed227b8463fa5f7445a30a8ed3936f7edd
SHA257 <48187868+SHA257@users.noreply.github.com>
2019-09-28 19:33:21 -0400

    Update MB_Render.js

--- MB_Render.js
+++ MB_Render.js
@@ -3,7 +3,7 @@ const div = document.getElementById('my-money-button');
     to: "3052",
     amount: "0.01",
     currency: "USD",
-    label: "Unlock",
+    label: "Play",
     clientIdentifier: "70b4e966e422161cae4eafb498766d3a",
     buttonId: "1554147627294",
     buttonData: "{}",
